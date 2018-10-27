594bf73345820bf947145c9b47d6e27ea0831730
Eduardo S. Pereira <pereira.somoza@gmail.com>
2018-10-27 11:26:06 -0300

    Atualizando controll

--- lib/ControlPanel/ControlPanel.cpp
+++ lib/ControlPanel/ControlPanel.cpp
@@ -4,7 +4,13 @@
 
 
 
-ControlPanel::ControlPanel(const int xpin, const int ypin, const int btnpin){
+ControlPanel::ControlPanel(const int xpin, const int ypin, const int btnpin,
+                           int xmin, int xmax, int ymin, int ymax){
+
+    _xmin = xmin;
+    _xmax = xmax;
+    _ymin = ymin;
+    _ymax = ymax;
 
     _xpin = xpin;
     _ypin = ypin;
@@ -29,6 +35,32 @@ void ControlPanel::unregisterObserver(){
 
 }
 
+void ControlPanel::_update(){
+  int tmp;
+  for(int i = 0 ; i < 5; i++){
+    tmp = digitalRead(_btnpin);
+  }
+
+  tmp /= 5;
+
+  if(tmp == 0){
+    panel_values.is_btn_pressed = false;
+    panel_values.time_btn_pressed = 0;
+  }else{
+    panel_values.is_btn_pressed = true;
+    if(panel_values.time_btn_pressed == 0){
+      panel_values.time_btn_pressed = millis();
+      millis_tr_pressed = millis();
+    }else{
+      panel_values.time_btn_pressed = millis() - millis_tr_pressed;
+    }
+  }
+
+  panel_values.x_value = map(analogRead(_xpin), 0, 1023, _xmin, _xmax);
+  panel_values.y_value = map(analogRead(_ypin), 0, 1023, _ymin, _ymax);
+
+}
+
 
 void ControlPanel::_notifyObserver(){
     if(observer != nullptr){
@@ -38,5 +70,6 @@ void ControlPanel::_notifyObserver(){
 }
 
 void ControlPanel::update(){
+  _update();
     _notifyObserver();
 }
